24b82407825e2d4180d9c6e28aa38f61c66cc86a
yukhymets <yukhymets.vadym@gmail.com>
2022-07-24 23:32:38 +0200

    polamav

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,20 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      
+      
+      modal: document.querySelector("[data-modal]"),
+       body: document.querySelector  ("[data-body"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      refs.body.classList.toggle("no-scroll");
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+      
+    }
+  })();
